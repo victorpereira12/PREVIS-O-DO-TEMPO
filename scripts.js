4403328dd5d2a47eae1cfd62384f66b2dada2377
Victor Pereira <victor386pereira@gmail.com>
2024-04-02 23:09:02 -0300

    FINISH

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const key = "82283bded69d5fcac119eacfe3733aee"
+
+
+function colocarNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+}
+async function buscarCidade(cidade){
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+    colocarNaTela(dados)    
+}
+
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+    buscarCidade(cidade)
+}
